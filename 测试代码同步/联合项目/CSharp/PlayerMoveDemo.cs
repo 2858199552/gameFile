203812e17243af1970f581ef6635b9272a7e6e15
zhongqi <58109730+2858199552@users.noreply.github.com>
2020-01-22 22:29:21 +0800

    联合项目
    创作者@幻旻矜寂
    估计：左右跑动，跳，二段跳，突进，动画逻辑控制......

---
+++ 测试代码同步/联合项目/CSharp/PlayerMoveDemo.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+
+public class PlayerMoveDemo : MonoBehaviour
+{
+    [HideInInspector] public Rigidbody2D Rigid;  //刚体组件(不用设置)
+    [HideInInspector] public float LRMove;//左右键检测(不用设置
+    [HideInInspector] private Animator anim;
+    public float MoveADDSpeed =0.5f;  //冲刺速度
+    public float PlayerLeft;    //玩家左侧碰撞
+    public float PlayerRight;  //玩家右侧碰撞
+    public float Speed = 10;  //移动速度
+    public LayerMask mask; //冲刺检测层
+    public float length; //冲刺距离
+    public bool HowMove; //移动方式(勾选普通，否则物理)
+    private int ADKeyDown;  //左右键计数(不用设置)
+    private int index;             //次数判断(不用设置)
+    private float time;  //内置计时器(不用设置)
+    private bool IsMoveADD; //是否正在冲刺;
+    private Vector2 vector; //冲刺目标位置记录;
+
+    private void Start()
+    {
+        Rigid = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+    }
+    private void OnEnable()
+    {
+        IsMoveADD = false;
+    }
+    private void Update()
+    {
+        if (!IsMoveADD)
+        {
+            if (time > 0.4f) index = 0;
+            if (index != 0)
+            {
+                time += Time.deltaTime;
+            }
+            else time = 0;
+            LRMove = Input.GetAxisRaw("Horizontal");
+            AnimationMove();
+            if (LRMove != 0)
+            {
+                if (HowMove)
+                    NormalMove();
+                else
+                    RigidMove();
+            }
+            KeyDowns();
+        }
+    }
+    private void RigidMove() //惯性移动
+    {
+        DirectionMove();
+        Rigid.AddForce(transform.right * Speed * Mathf.Abs(LRMove) * Time.deltaTime);
+    }
+    private void NormalMove() //正常移动
+    {
+        DirectionMove();
+        transform.position += transform.right * Speed * Time.deltaTime * Mathf.Abs(LRMove);
+    }
+    private void DirectionMove() //转向
+    {
+        if (LRMove < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+    private void AnimationMove()
+    {
+        anim.SetBool("IsMove", LRMove != 0);
+    }
+    private void KeyDowns()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (ADKeyDown != 1) index = 0;
+            ADKeyDown = 1;
+            index += 1;
+            if (index == 2 && ADKeyDown == 1)
+            {
+                anim.SetBool("MoveAdd", true);
+                if (HowMove)
+                    Delivery(PlayerLeft);
+                else
+                    Delivery();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (ADKeyDown != 2) index = 0;
+            ADKeyDown = 2;
+            index += 1;
+            if (index == 2 && ADKeyDown == 2)
+            {
+                anim.SetBool("MoveAdd", true);
+                if (HowMove)
+                    Delivery(PlayerRight);
+                else
+                    Delivery();
+            }
+        }
+    }
+    private void Delivery(float direction)
+    {
+        RaycastHit2D hit;
+        {
+            if (hit = Physics2D.Raycast(this.transform.position, this.transform.right, length, mask.value)) 
+                vector = new Vector2(hit.point.x - direction, this.transform.position.y);
+            else vector=this.transform.position + this.transform.right * length;
+        }
+        StartCoroutine("MoveADD");
+    }
+    private void Delivery()
+    {
+        Rigid.AddForce(transform.right *length);
+    }
+    private IEnumerator MoveADD()
+    {
+        IsMoveADD = true;
+        bool BigOrSmall;
+        if (vector.x < this.transform.position.x) BigOrSmall = true;
+        else BigOrSmall = false;
+        for (; (this.transform.position.x - vector.x > 0.5f && BigOrSmall) || (vector.x - this.transform.position.x > 0.5f && !BigOrSmall);)
+        {
+            yield return new WaitForFixedUpdate();
+            this.transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(vector.x, this.transform.position.y), MoveADDSpeed);
+        }
+        IsMoveADD = false;
+        anim.SetBool("MoveAdd", false);
+    }
+}
---
+++ 测试代码同步/联合项目/CSharp/PlayerMoveDemo2.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+
+public class PlayerMoveDemo2 : MonoBehaviour
+{
+    [HideInInspector] public Rigidbody2D Rigid;    //刚体组件(不用设置)
+    [HideInInspector] public float WSMove;            //WS检测(不用设置)
+    [HideInInspector] public Animator anim;          //Animator动画组件(不用设置)
+    [HideInInspector] public bool IsPlace;            //是否在地面上(不用设置)
+    [HideInInspector] public float InPlaceTime;  //跳跃记时(不用设置)
+    [HideInInspector] public RaycastHit2D hit;        //射线返回(不用设置)
+    [Range(0, 5)] public float SpeedDown;            //下落速度
+    [Range(0.001f,0.050f)] public float interval = 0.02f;  //跳跃降落与再次触发间隔(慎改！)
+    [Range(0.01f,0.5f)] public float Percentage=0.1f;  //每帧上升百分比
+    public float Height;             //跳跃高度
+    public LayerMask maskUp;    //撞击层检测
+    public LayerMask groundMask; //地面层检测
+    public LayerMask maskWall; //墙壁检测层
+    public float GameObjectHeight;   //物体高度
+    public float GameObjectInPlace; //物体距离地面高度
+    public float length;  //墙壁碰撞射线长度
+    public int Count=30;                    //跳跃帧(一般不用管)
+    private int index;                  //跳跃次数(不用设置)
+    private bool IsInWall;   //是否降落墙边
+    private void Start()
+    {
+        Rigid = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        WSMove = Input.GetAxisRaw("Vertical");
+        NormalJump();
+    }
+    private void OnEnable()
+    {
+        IsPlace = true;
+    }
+    private void NormalJump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            index += 1;
+            if (IsPlace)
+            {
+                IsPlace = false;
+                StartCoroutine("JumpUse");
+                anim.SetFloat("MoveWS", 1);
+                anim.SetBool("IsInPlace", false);
+            }
+        }
+    }
+    private IEnumerator JumpUse()
+    {
+        Again:
+        float MoveUP = this.transform.position.y + Height;
+        InPlaceTime = 0;
+        Rigid.gravityScale = 0;
+        for (int i = 0; i <Count; i++)
+        {
+            if (Physics2D.Raycast(this.transform.position, transform.transform.up, GameObjectHeight, maskUp.value)) break;
+            if (index == 2)
+            {
+                index += 1;
+                goto Again;
+            }
+            yield return new WaitForFixedUpdate();
+            this.transform.position=Vector2.Lerp(this.transform.position, new Vector2(this.transform.position.x, MoveUP),Percentage);
+            InPlaceTime +=0.1f;
+        }
+        Rigid.gravityScale = SpeedDown;
+        for (;InPlaceTime>0;)
+        {
+            if (Physics2D.Raycast(this.transform.position, -transform.transform.up, GameObjectInPlace, groundMask.value)) break;
+             else if (Physics2D.Raycast(this.transform.position, transform.transform.right, length, maskWall.value)) break;
+            InPlaceTime -= interval;
+            yield return new WaitForFixedUpdate();
+            if (index == 2)
+            {
+                index += 1;
+                goto Again;
+            }
+        }
+        IsPlace = true;
+        index = 0;
+        anim.SetBool("IsInPlace",true);
+        anim.SetFloat("MoveWS", 0);
+    }
+}
